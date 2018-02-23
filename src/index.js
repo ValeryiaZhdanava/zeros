f396ad267ec114010293ccb61d57b33e247f60cf
Valeryia Zhdanava <vzhd@ciklum.com>
2018-02-23 20:44:16 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
-}
+    let countZero = 0;
+    let pwr;
+    let x;
+    for (let y = 1; y <= 1000; y++) {
+        pwr = Math.pow(5, y + .0);
+        if (number <= pwr) {
+            x = y;
+            break;
+        }
+    }
+    for (let i = 1; i <= x; i++) {
+        countZero += (~~(number / Math.pow(5, i + .0)));
+    }
+    return countZero;
+};
